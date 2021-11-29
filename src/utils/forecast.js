0b8653ed9675d9f2ed2c1b3db598aea59407096f
Naín Pedroza <jesusnain.pedroza.contractor@bbva.com>
2021-11-29 12:40:33 -0600

    Small changes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,14 +2,13 @@ const request = require('postman-request')
 
 const forecast = (lat, long,callback) =>{
     const url = 'http://api.weatherstack.com/current?access_key=848d6599abab78d9632c66f71fcba3a2&query='+lat+','+long+'&units=m'
-    console.log(url)
     request({ url, json: true}, (error, {body}) => {
         if (error){
             callback('Unable to connect to weather services!', undefined)
         } else if (body.error) {
             callback('Unable to find location, try another aaaasearch.',undefined)
         } else {
-             callback(undefined,body.current.weather_descriptions + " and it is currently " + body.current.temperature + ' and it it ' + body.current.precip+'% of rain.')
+             callback(undefined,body.current.weather_descriptions + " and it is currently " + body.current.temperature + ' and it is ' + body.current.precip+'% of rain.')
         }
     })
 }
